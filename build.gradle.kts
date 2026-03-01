e16fcb143bf998cfa674f61dd967c82e8f00f82a
tuancoltech <tuancoltech@gmail.com>
2026-03-01 22:40:18 +0700

    * Enable Firebase Crashlytics/Analytics * Restore mistakenly deleted `telegram_link` string

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,5 +7,7 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
